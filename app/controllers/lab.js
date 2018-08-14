1d213f4cb7fa725c906a050ab18ce8cf7bf5688f
MarcosZarraga <A01271642@itesm.mx>
2018-08-14 08:23:26 -0500

    Clase 14 agosto

---
+++ app/controllers/lab.js
@@ -0,0 +1,103 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+	cat: 'pelusa',
+
+	tweets :[
+	],
+
+	weekDays : [
+	{
+		day: 'monday',
+		number: '1',
+		'mi propiedad':'hola'
+	},
+	{
+		day: 'martes',
+		number: '2',
+		'mi propiedad':'hola'
+	},
+	{
+		day: 'miercoles',
+		number: '3',
+		'mi propiedad':'hola'
+	},
+	{
+		day: 'jueves',
+		number: '4',
+		'mi propiedad':'hola'
+	},
+	{
+		day: 'viernes',
+		number: '5',
+		'mi propiedad':'hola'
+	},
+	{
+		day: 'sabado',
+		number: '6',
+	'mi propiedad':'hola'
+	},
+	{
+		day: 'domingo',
+		number: '7',
+		'mi propiedad':'hola'
+	}
+
+
+
+	],
+
+	actions:
+	 {
+		doSomething() 
+		{
+			var cat= this.get('cat');
+			console.log(cat);
+		},
+
+		valdiarUsuario()
+		{
+			var usuario= this.get('user');
+			if(usuario=="A01271642" && this.get('password')==="1234"){
+				console.log("usuario valido");
+			}
+			else {
+				console.log("Usuario invalido");
+			}
+
+			this.set('user',"");
+			this.set('password',"");
+			
+		},
+
+
+		myLuckyDay(){
+			var weekDays= this.get('weekDays');
+			var n = Math.floor(Math.random() * weekDays.length);
+			console.log(weekDays.objectAt(n)['day']);
+		},
+		addDay(){
+			var newDay=this.get('newDay');
+			var weekDays=this.get('weekDays');
+
+			weekDays.pushObject({
+				day: newDay,
+				//number:Math.floor(Math.random()*100),
+				'mi propiedad': 'hola'
+			})
+		},
+
+		addTweet(){
+			var newTweet=this.get('newTweet');
+			var tweets=this.get('tweets');
+
+			tweets.pushObject({
+				'usuario' : 'Marcos',
+				'descripcion':  newTweet
+			})
+			this.set('newTweet', "")
+
+		}
+
+	}
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+this.route('lab',{path :'/lab'});
 });
 
 export default Router;
