00938a655f03e9b34c2d195cf246ec50de234e57
MarcosZarraga <A01271642@itesm.mx>
2018-11-06 08:23:27 -0600

    clase 6 de nov

--- app/models/food.js
+++ app/models/food.js
@@ -1,11 +1,15 @@
 import DS from 'ember-data';
+// import {computed} from '@ember/object'; 
 
 export default DS.Model.extend({
 	name:DS.attr('string'),
 	servings:DS.attr('number'),
 	meal:DS.belongsTo('meal'),
-	product:DS.belongsTo('product')
+	product:DS.belongsTo('product'),
 
+	calories: Ember.computed('servings', 'product.calories', function(){
+		return this.servings * this.product.get('calories')
+	})
 
 
 });
--- app/models/meal.js
+++ app/models/meal.js
@@ -1,7 +1,12 @@
 import DS from 'ember-data';
+// import {computed} from '@ember/object';
 
 export default DS.Model.extend({
-	name:DS.attr('string')
-
-
+	name:DS.attr('string'),
+	createdAt: DS.attr('date'),
+	foods: DS.hasMany('foods'),
+	foodCalories: Ember.computed('foods.@each.calories', function(){
+		return this.foods.mapBy('calories');
+	}),
+	totalCalories: Ember.computed.sum('foodCalories')
 });
